aef69121fe92ff98c037c039090b854b2989c965
Andrea Lovewell <andy.lovewell@gmail.com>
2019-01-06 04:08:56 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,12 +4,7 @@
 int = gets.chomp
 
 def fizzbuzz(int)
-  if (int % 3 ==0)
+  if (int % 3 == 0)
     return "Fizz"
-  elsif (int % 5 == 0)
-    return "Buzz"
-  elsif (int % 3 == 0 && int % 5 ==0)
-    return "FizzBuzz"
-  else
-    return nil
   end
+end
